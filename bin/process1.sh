838c31f75f583ed0d47772c5aae9ea95aa4108b2
randy <r@ngsfed.com>
2015-01-17 19:57:41 -0500

    Fixed process1.sh so it correctly puts data in the correct output directory.

--- bin/process1.sh
+++ bin/process1.sh
@@ -2,4 +2,4 @@
 
 for file in `ls processing0.1/*`; do   
    jq -c  '.created_at as $global_created_at | .id  as $id | .asin as $asin | .item_details[] | { "global_created_at" : $global_created_at, "child_created_at" : .created_at, "id" : $id, "asin" : $asin, "qty" : .quantity, "price" : .price , "sales_rank" : .sales_rank}' $file
-   done > flattened.json
+   done > processing1/flattened.json
